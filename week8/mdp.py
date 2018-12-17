fix: Fixed mdp calculation

week8/mdp.py
@@ -9,15 +9,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # create the map
-PROB_DESIRED = 0.99
+PROB_DESIRED = 1.0
 PROB_OFF = (1 - PROB_DESIRED) / 2
 PROB_BACK = 0
-STANDARD_REWARD = -1
-HOLE_REWARD = -100
-BORDER_REWARD = 0
-IN_PLACE_REWARD = -1
-MAX_REWARD = 100
-GAMMA = 0.99
+STANDARD_REWARD = -0.01
+HOLE_REWARD = -1
+BORDER_REWARD = -1000
+IN_PLACE_REWARD = 0
+MAX_REWARD = 1
+GAMMA = 1.0
 
 def iterate(val_in, reward):
     # val_in is a 2d array
@@ -41,14 +41,24 @@ def iterate(val_in, reward):
             val_left = val_in[i, j - 1] if reward[i, j - 1] != BORDER_REWARD else val_stay
             val_right = val_in[i, j + 1] if reward[i, j + 1] != BORDER_REWARD else val_stay
             
+#            r_up = reward[i - 1, j] if reward[i - 1, j] != BORDER_REWARD else IN_PLACE_REWARD
+#            r_down = reward[i + 1, j] if reward[i + 1, j] != BORDER_REWARD else IN_PLACE_REWARD
+#            r_left = reward[i, j - 1] if reward[i, j - 1] != BORDER_REWARD else IN_PLACE_REWARD
+#            r_right = reward[i, j + 1] if reward[i, j + 1] != BORDER_REWARD else IN_PLACE_REWARD
+
+            r_here = reward[i, j]
+
+#            val_up += r_up
+#            val_down += r_down
+#            val_left += r_left
+#            val_right += r_right
             
             # test up
             prob_up = val_up * PROB_DESIRED \
                 + val_down * PROB_BACK \
                 + val_left * PROB_OFF \
                 + val_right * PROB_OFF
-            r_up = reward[i - 1, j] if reward[i - 1, j] != BORDER_REWARD else IN_PLACE_REWARD
-            vf_up = r_up + GAMMA * prob_up
+            vf_up = prob_up #r_up + prob_up
             if vf_up > val:
                 val = vf_up
                 policy = 0
@@ -57,8 +67,7 @@ def iterate(val_in, reward):
                 + val_down * PROB_DESIRED \
                 + val_left * PROB_OFF \
                 + val_right * PROB_OFF
-            r_down = reward[i + 1, j] if reward[i + 1, j] != BORDER_REWARD else IN_PLACE_REWARD
-            vf_down = r_down + GAMMA * prob_down
+            vf_down = prob_down # r_down + prob_down
             if vf_down > val:
                 val = vf_down
                 policy = 1
@@ -67,8 +76,7 @@ def iterate(val_in, reward):
                 + val_down * PROB_OFF \
                 + val_left * PROB_DESIRED \
                 + val_right * PROB_BACK
-            r_left = reward[i, j - 1] if reward[i, j - 1] != BORDER_REWARD else IN_PLACE_REWARD
-            vf_left = r_left + GAMMA * prob_left
+            vf_left = prob_left #r_left + prob_left
             if vf_left > val:
                 val = vf_left
                 policy = 2
@@ -77,16 +85,21 @@ def iterate(val_in, reward):
                 + val_down * PROB_OFF \
                 + val_left * PROB_BACK \
                 + val_right * PROB_DESIRED
-            r_right = reward[i, j + 1] if reward[i, j + 1] != BORDER_REWARD else IN_PLACE_REWARD
-            vf_right = r_right + GAMMA * prob_right
+            vf_right = prob_right #r_right + prob_right
             if vf_right > val:
                 val = vf_right
                 policy = 3
                     
-            if reward[i, j] == MAX_REWARD or reward[i, j] == HOLE_REWARD:
+              
+            if r_here == MAX_REWARD or r_here == HOLE_REWARD:
                 val = 0
                 policy = 4
-            val_out[i, j] =  val
+            if r_here == BORDER_REWARD:
+                val = 0
+                policy = 4
+                r_here = 0
+
+            val_out[i, j] =  GAMMA * val + r_here
             policy_out[i, j] = policy
     return (val_out, policy_out)
 
@@ -106,11 +119,11 @@ def printArray(arr):
 def displayValuesAndControlPolicy(val, policy=None):
     fig, ax = plt.subplots()
     ax.imshow(val, cmap = plt.get_cmap('gray'))
-    if policy is not None:
-        max_i = policy.shape[0]
-        max_j = policy.shape[1]
-        for i in range(max_i):
-            for j in range(max_j):    
+    max_i = val.shape[0]
+    max_j = val.shape[1]
+    for i in range(max_i):
+        for j in range(max_j): 
+            if policy is not None:
                 character = ''
                 policy_val = policy[i, j]
                 if policy_val == 0:
@@ -123,7 +136,10 @@ def displayValuesAndControlPolicy(val, policy=None):
                     character = '>'
                 elif policy_val == 4:
                     character = 'o'
-                ax.text(j - 0.3, i + 0.3, character, size='12', color='r')
+            else:
+                character = '{0:.2f}'.format(val[i, j])
+            ax.text(j - 0.3, i + 0.3, character, 
+                    size='10', color='k', backgroundcolor='w')
 
 
 # Create the intial arrays
@@ -131,20 +147,22 @@ def displayValuesAndControlPolicy(val, policy=None):
 #reward[GOAL] = MAX_REWARD
 #reward[4, 4:12] = HOLE_REWARD
 #reward[4:8, 4:12] = HOLE_REWARD  
-reward = np.ones((10, 22)) * STANDARD_REWARD
+reward = np.ones((5, 6)) * STANDARD_REWARD
+
+reward[1, 4] = 1
+reward[2, 4] = -1
+reward[2, 2] = BORDER_REWARD
 
 reward[:, 0] = BORDER_REWARD
 reward[:, -1] = BORDER_REWARD
 reward[0, :] = BORDER_REWARD
 reward[-1, :] = BORDER_REWARD
 
-reward[(5, 3)] = MAX_REWARD
-reward[4, 5:12] = HOLE_REWARD
-reward[6:8, 5:12] = HOLE_REWARD
+
 
 printArray(reward)
 
-val_old = np.ones(reward.shape) * -1
+val_old = np.ones(reward.shape) * 0
 policy = None
 #printArray(val_old)
 #print("Going to iterate")
@@ -155,10 +173,12 @@ while iteration < 1000:
         print("Made it at iteration {}".format(iteration))
         break
     val_old = val_new
+    displayValuesAndControlPolicy(val_old)
     iteration += 1
 #    printArray(val_old)
 #    printArray(policy)
 printArray(val_old)
 printArray(policy)
 
+displayValuesAndControlPolicy(val_old)
 displayValuesAndControlPolicy(val_old, policy)